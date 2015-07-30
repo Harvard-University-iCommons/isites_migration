58d4c2f88ed9517e41ab6b0e874c6372768e96ac
Douglas Hall <douglas_hall@harvard.edu>
2015-07-30 17:11:47 +0000

    Completed import_files command

---
+++ file_service/management/commands/import_files.py
@@ -0,0 +1,137 @@
+import logging
+import json
+import time
+import ssl
+if hasattr(ssl, '_create_unverified_context'):
+    ssl._create_default_https_context = ssl._create_unverified_context
+
+from optparse import make_option
+
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+
+from boto.s3.connection import S3Connection
+
+from canvas_sdk.methods import content_migrations, files
+
+from icommons_common.canvas_utils import SessionInactivityExpirationRC
+
+
+logger = logging.getLogger(__name__)
+SDK_CONTEXT = SessionInactivityExpirationRC(**settings.CANVAS_SDK_SETTINGS)
+
+
+class Command(BaseCommand):
+    help = 'Imports iSites file repository export to Canvas'
+    option_list = BaseCommand.option_list + (
+        make_option(
+            '--keyword',
+            action='store',
+            dest='keyword',
+            default=None,
+            help='Provide an iSites keyword'
+        ),
+        make_option(
+            '--canvas_course_id',
+            action='store',
+            dest='canvas_course_id',
+            default=None,
+            help='Provide a Canvas course ID'
+        ),
+        make_option(
+            '--csv',
+            action='store',
+            dest='csv_path',
+            default=None,
+            help='Provide the path to a csv file containing iSites keyword/Canvas course ID pairs'
+        ),
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(Command, self).__init__(*args, **kwargs)
+        self.connection = S3Connection(settings.AWS_ACCESS_KEY_ID, settings.AWS_ACCESS_KEY)
+        self.bucket = self.connection.get_bucket(settings.AWS_EXPORT_BUCKET_SLIDE_TOOL, validate=False)
+        self.canvas_progress_urls = []
+
+    def handle(self, *args, **options):
+        keyword = options.get('keyword')
+        canvas_course_id = options.get('canvas_course_id')
+        csv_path = options.get('csv_path')
+
+        if csv_path:
+            self._import_csv(csv_path)
+        elif keyword and canvas_course_id:
+            self._import_isite(keyword, canvas_course_id)
+        else:
+            raise CommandError(
+                'You must provide either the --keyword and --canvas_course_id options or the --csv option.'
+            )
+
+        import_count = len(self.canvas_progress_urls)
+        while self.canvas_progress_urls:
+            time.sleep(2)
+            completed_imports = []
+            for progress_url in self.canvas_progress_urls:
+                progress = json.loads(SDK_CONTEXT.session.request('GET', progress_url).text)
+                if progress['workflow_state'] == 'completed':
+                    completed_imports.append(progress_url)
+            for progress_url in completed_imports:
+                self.canvas_progress_urls.remove(progress_url)
+            count_processing = len(self.canvas_progress_urls)
+            if count_processing:
+                logger.info(
+                    "%d Canvas imports complete, %d still processing",
+                    len(completed_imports),
+                    count_processing
+                )
+
+        logger.info("Completed import of %d iSites file exports.", import_count)
+
+    def _import_csv(self, csv_path):
+        logger.info("Importing iSites file exports from csv %s", csv_path)
+        try:
+            with open(csv_path, 'rb') as csv_file:
+                for row in csv.reader(csv_file):
+                    self.import_isite(row[0], row[1])
+        except (IOError, IndexError):
+            raise CommandError("Failed to read csv file %s", csv_path)
+
+    def _import_isite(self, keyword, canvas_course_id):
+        export_file_url = self._get_export_s3_url(keyword)
+        logger.info("Importing iSites file export from %s to Canvas course %s", export_file_url, canvas_course_id)
+        response = json.loads(content_migrations.create_content_migration_courses(
+            SDK_CONTEXT,
+            canvas_course_id,
+            'zip_file_importer',
+            settings_file_url=self._get_export_s3_url(keyword),
+            settings_folder_id=self._get_root_folder_for_canvas_course(canvas_course_id)['id']
+        ).text)
+        progress_url = response['progress_url']
+        self.canvas_progress_urls.append(progress_url)
+        logger.info(
+            "Created Canvas content migration %s for import from %s to Canvas course %s",
+            progress_url,
+            export_file_url,
+            canvas_course_id
+        )
+
+    def _get_export_s3_url(self, keyword):
+        try:
+            key = self.bucket.get_key("%s.zip" % keyword)
+        except boto.exception.S3ResponseError:
+            raise CommandError(
+                "iSites file export for keyword %s does not exist in S3 bucket %s", keyword, self.bucket.name
+            )
+
+        return key.generate_url(settings.AWS_EXPORT_DOWNLOAD_TIMEOUT_SECONDS)
+
+    def _get_root_folder_for_canvas_course(self, canvas_course_id):
+        folders = json.loads(files.get_folder_courses(
+            SDK_CONTEXT,
+            canvas_course_id,
+            ''
+        ).text)
+        for folder in folders:
+            if not folder.get('parent_folder_id'):
+                return folder
+        return None
