43b10251b4e8ad5ec7116f4350b5dc7f311e5e0b
Douglas Hall <douglas_hall@harvard.edu>
2015-08-07 14:41:51 +0000

    Fix S3 upload path

--- file_service/management/commands/export_files.py
+++ file_service/management/commands/export_files.py
@@ -140,11 +140,8 @@ class Command(BaseCommand):
 
             zip_path_index = len(settings.EXPORT_DIR) + 1
             keyword_export_path = os.path.join(settings.EXPORT_DIR, settings.CANVAS_IMPORT_FOLDER_PREFIX + keyword)
-            zip_file = "%s%s.zip" % (settings.CANVAS_IMPORT_FOLDER_PREFIX, keyword)
-            z_file = zipfile.ZipFile(
-                os.path.join(settings.EXPORT_DIR, zip_file),
-                'w'
-            )
+            zip_filename = os.path.join(settings.EXPORT_DIR, "%s%s.zip" % (settings.CANVAS_IMPORT_FOLDER_PREFIX, keyword))
+            z_file = zipfile.ZipFile(zip_filename, 'w')
             for root, dirs, files in os.walk(keyword_export_path):
                 for file in files:
                     file_path = os.path.join(root, file)
@@ -156,7 +153,7 @@ class Command(BaseCommand):
             export_key.key = "%s.zip" % keyword
             export_key.set_metadata('Content-Type', 'application/zip')
             keyword_export_file = os.path.join(settings.EXPORT_DIR, export_key.key)
-            export_key.set_contents_from_filename(zip_file)
+            export_key.set_contents_from_filename(zip_filename)
             logger.info("Uploaded file export for keyword %s to S3 Key %s", keyword, export_key.key)
 
             os.remove(keyword_export_file)
